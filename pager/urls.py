0e106f2d1e9fcf85d3442e4b8bda542e56620d36
AmoahDevLabs <amoahdevlabs@gmail.com>
2020-07-30 16:15:15 +0000

    Partial commit

--- pager/urls.py
+++ pager/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     # path('', views.index, name='home'),
     path('', views.index, {'page_name': ''}, name='home'),
+    path('contact', views.contact, name='contact'),
     path('<str:page_name>', views.index, name='index'),
 ]
