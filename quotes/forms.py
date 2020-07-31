79f5e0ed0af94fd9053eace7675a170af00a1275
AmoahDevLabs <amoahdevlabs@gmail.com>
2020-07-31 17:43:17 +0000

    Partials on *.css, *.html, *.py

--- quotes/forms.py
+++ quotes/forms.py
@@ -1,4 +1,20 @@
 from django import forms
 from django.forms import ModelForm
+from .models import Quote
+
+
+class QuoteForm(ModelForm):
+    required_css_class = 'required'
+
+    class Meta:
+        model = Quote
+
+        fields = [
+            'name', 'position', 'company', 'address',
+            'phone_number', 'email', 'website', 'description',
+            'site_status', 'priority', 'job_file',
+        ]
+
+
 
 
